a075d5aaaafdfd071f39507681cc12fb5378916b
artiom.levchuk <Nadoelo123>
2018-02-25 14:07:12 +0300

    add fragments

--- app/src/main/java/by/unicard/unicard/ui/activity/MainActivity.java
+++ app/src/main/java/by/unicard/unicard/ui/activity/MainActivity.java
@@ -4,39 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import by.unicard.unicard.R;
+import by.unicard.unicard.ui.fragment.CategoriesFragment;
+import by.unicard.unicard.ui.fragment.DiscountsFragment;
+import by.unicard.unicard.ui.fragment.MapFragment;
+import by.unicard.unicard.ui.fragment.ProfileFragment;
 import by.unicard.unicard.utils.PreferencesUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_discounts:
-                    mTextMessage.setText(R.string.title_discounts);
-                    return true;
-                case R.id.navigation_map:
-                    mTextMessage.setText(R.string.title_map);
-                    return true;
-                case R.id.navigation_categories:
-                    mTextMessage.setText(R.string.title_categories);
-                    return true;
-                case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +32,37 @@ public class MainActivity extends AppCompatActivity {
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment newFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.navigation_discounts:
+                        newFragment = new DiscountsFragment();
+                        mTextMessage.setText(R.string.title_discounts);
+                        return true;
+                    case R.id.navigation_map:
+                        newFragment = new MapFragment();
+                        mTextMessage.setText(R.string.title_map);
+                        return true;
+                    case R.id.navigation_categories:
+                        newFragment = new CategoriesFragment();
+                        mTextMessage.setText(R.string.title_categories);
+                        return true;
+                    case R.id.navigation_profile:
+                        newFragment = new ProfileFragment();
+                        mTextMessage.setText(R.string.title_profile);
+                        return true;
+                }
+                replaceFragment(newFragment);
+                return true;
+            }
+        });
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragment).commit();
     }
 
 }
---
+++ app/src/main/java/by/unicard/unicard/ui/fragment/DiscountsFragment.java
@@ -0,0 +1,22 @@
+package by.unicard.unicard.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public class DiscountsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+}
